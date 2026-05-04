3d2cd1afd9f6bc4267cdbfe76026fe6a8d12b92a
sarpanch132 <68178340+sarpanch132@users.noreply.github.com>
2026-05-04 07:41:54 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-
-const CACHE = "cracktrack-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-});
